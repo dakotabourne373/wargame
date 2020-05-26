0fefe7cc8c3c96bed25329b785df54ab2c94b745
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-05-26 17:35:44 -0400

    Updates

---
+++ precache-manifest.e2ecf8211420f9a823c897e73f62b821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b56590c2f0d766328e73160395d945",
+    "url": "/wargame/index.html"
+  },
+  {
+    "revision": "264a8396bf13af0c3302",
+    "url": "/wargame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39f49a211ec77b90f476",
+    "url": "/wargame/static/js/2.cfe864b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wargame/static/js/2.cfe864b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264a8396bf13af0c3302",
+    "url": "/wargame/static/js/main.dea98aa6.chunk.js"
+  },
+  {
+    "revision": "24443f649c0e6e3a939f",
+    "url": "/wargame/static/js/runtime-main.1fea36bb.js"
+  }
+]);
